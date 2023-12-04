908b7e6d40650f3598ef538e68dff08ee88d609d
yacine belaid <yacine.bld.belaid@gmail.com>
2023-12-04 15:29:08 -0500

    preparation simulation

--- heatsim.cpp
+++ heatsim.cpp
@@ -24,6 +24,13 @@ using namespace std;
 using namespace mfem;
 
 int main(int argc, char *argv[]) {
+
+ int num_procs = 1; // Défaut
+    if (argc > 1) {
+        num_procs = std::stoi(argv[1]);
+    }
+ const char *output = "Heatsim";
+    std::string output_name = output + std::to_string(num_procs);
   // 1. Initialize MPI and HYPRE.
   auto start_program = std::chrono::steady_clock::now();
   Mpi::Init();
@@ -206,7 +213,7 @@ int main(int argc, char *argv[]) {
   double dofs_per_second = size / (program_duration / 1000.0);
 
   //ECRITURE
-  std::ofstream outputFile("timeMeasurement.csv");
+  std::ofstream outputFile(output_name + "_timeMeasurement.csv");
   if (outputFile.is_open()) {
         outputFile << "Resultats : " <<  std::endl;
         outputFile << "Raffinement : " << duration_mesh << "milliseconds." << std::endl;
