498e572291fcbb31b3a143e0f79582c22f20b75d
« gj591923 » <gj591923@ens.uqam.ca>
2023-12-12 01:43:16 +0000

    update

--- Boucle.sh
+++ Boucle.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-for i in 1 2 4 16 32 64; do
-	
-	for j in 0 1 2 3 4; do
-	sbatch --ntasks=${i} work.slurm ${i} ${j}
-	done
+for i in 1 2 4 8 16 32 64; do
+    for j in 1 2 3 4; do
+        sbatch --ntasks=${i} work2.slurm ${i} ${j}
+    done
 done
